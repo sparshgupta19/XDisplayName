398e1c5694e371b650dac53a5539024392952093
Sparsh <sparsh2000gupta@gmail.com>
2024-01-28 03:30:41 +0530

    r commit

--- src/App.js
+++ src/App.js
@@ -16,34 +16,39 @@ function FullNameDisplay() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (firstName.trim() !== '' || lastName.trim() !== '') {
+    if (firstName.trim() !== '' && lastName.trim() !== '') {
       setFullName(`${firstName} ${lastName}`);
+    } else {
+      setFullName('');
     }
   };
 
-  return (<div>
-    <form onSubmit={handleSubmit}>
-      <h1>Full Name Display</h1>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Full Name Display</h1>
 
-      <div>
-        <label>First Name:</label>
-        <input type="text" value={firstName} onChange={handleFirstNameChange} />
-      </div>
+        <div>
+          <label>First Name:</label>
+          <input type="text" value={firstName} onChange={handleFirstNameChange} />
+        </div>
 
-      <div>
-        <label>Last Name:</label>
-        <input type="text" value={lastName} onChange={handleLastNameChange} />
-      </div>
+        <div>
+          <label>Last Name:</label>
+          <input type="text" value={lastName} onChange={handleLastNameChange} />
+        </div>
 
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-      
-    </form>
-    <form>
-      {fullName && <p >Full Name: {fullName}</p>}
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
-      </div>
+
+      {fullName && (
+        <form>
+          <p>Full Name: {fullName}</p>
+        </form>
+      )}
+    </div>
   );
 }
 
